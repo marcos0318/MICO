fcc90c3963e99691dd192c2419b6d2d01c2f8edc
Jiaxin Bai <jbai@connect.ust.hk>
2022-11-19 19:36:23 +0800

    Fix: add more scripts

--- scripts/eval_tail_cn.sh
+++ scripts/eval_tail_cn.sh
@@ -9,3 +9,22 @@ CUDA_VISIBLE_DEVICES=1 python evaluate_tail.py --save_folder ./ckpts_conceptnet/
     --dropout \
     --batch_size 512
 
+CUDA_VISIBLE_DEVICES=1 python evaluate_tail.py --save_folder ./ckpts_conceptnet/k2/roberta_large \
+    --max_seq_length 32 \
+    --temp 0.07 \
+    --testfile ../preprocess/CN82k-Ind-valid.txt \
+    --model roberta-large \
+    --tokenizer_name roberta-large \
+    --dropout \
+    --batch_size 512
+
+
+CUDA_VISIBLE_DEVICES=1 python evaluate_tail.py --save_folder ./ckpts_conceptnet/k2/roberta_large \
+    --max_seq_length 32 \
+    --temp 0.07 \
+    --testfile ../preprocess/CN82k-Ind-test.txt \
+    --model roberta-large \
+    --tokenizer_name roberta-large \
+    --dropout \
+    --batch_size 512
+
