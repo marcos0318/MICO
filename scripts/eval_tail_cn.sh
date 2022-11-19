c0a9511377c5454609dc01acfcf9e2f570dd8475
Jiaxin Bai <jbai@connect.ust.hk>
2022-11-19 18:39:04 +0800

    debug file path

--- scripts/eval_tail_cn.sh
+++ scripts/eval_tail_cn.sh
@@ -3,7 +3,7 @@
 CUDA_VISIBLE_DEVICES=1 python evaluate_tail.py --save_folder ./ckpts_conceptnet/k2/roberta_large \
     --max_seq_length 32 \
     --temp 0.07 \
-    --testfile ./preprocess/CN82k-Ind-train.txt \
+    --testfile ../preprocess/CN82k-Ind-train.txt \
     --model roberta-large \
     --tokenizer_name roberta-large \
     --batch_size 512
@@ -11,7 +11,7 @@ CUDA_VISIBLE_DEVICES=1 python evaluate_tail.py --save_folder ./ckpts_conceptnet/
 CUDA_VISIBLE_DEVICES=1 python evaluate_tail.py --save_folder ./ckpts_conceptnet/k2/roberta_large \
     --max_seq_length 32 \
     --temp 0.07 \
-    --testfile ./preprocess/CN82k-Ind-valid.txt \
+    --testfile ../preprocess/CN82k-Ind-valid.txt \
     --model roberta-large \
     --tokenizer_name roberta-large \
     --batch_size 512
@@ -19,7 +19,7 @@ CUDA_VISIBLE_DEVICES=1 python evaluate_tail.py --save_folder ./ckpts_conceptnet/
 CUDA_VISIBLE_DEVICES=1 python evaluate_tail.py --save_folder ./ckpts_conceptnet/k2/roberta_large \
     --max_seq_length 32 \
     --temp 0.07 \
-    --testfile ./preprocess/CN82k-Ind-test.txt \
+    --testfile ../preprocess/CN82k-Ind-test.txt \
     --model roberta-large \
     --tokenizer_name roberta-large \
     --batch_size 512
