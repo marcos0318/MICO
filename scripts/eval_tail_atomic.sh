1fcf548a6f088b06f396c1894f04cd7729058147
Jiaxin Bai <jbai0318@gmail.com>
2022-11-20 11:48:37 +0800

    Fix: change file name

--- scripts/eval_tail_atomic.sh
+++ scripts/eval_tail_atomic.sh
@@ -1,6 +1,6 @@
 # train/dev/test
 
-CUDA_VISIBLE_DEVICES=0 python evaluate_tail.py --save_folder ./ckpts_atomic19/k2/roberta_large \
+CUDA_VISIBLE_DEVICES=0 python evaluate_tail.py --save_folder ./ckpts_atomic/k2/roberta_large \
     --max_seq_length 32 \
     --temp 0.07 \
     --testfile ../preprocess/ATOMIC-Ind-train.txt \
@@ -9,7 +9,7 @@ CUDA_VISIBLE_DEVICES=0 python evaluate_tail.py --save_folder ./ckpts_atomic19/k2
     --dropout \
     --batch_size 512
 
-CUDA_VISIBLE_DEVICES=0 python evaluate_tail.py --save_folder ./ckpts_atomic19/k2/roberta_large \
+CUDA_VISIBLE_DEVICES=0 python evaluate_tail.py --save_folder ./ckpts_atomic/k2/roberta_large \
     --max_seq_length 32 \
     --temp 0.07 \
     --testfile ../preprocess/ATOMIC-Ind-valid.txt \
@@ -18,7 +18,7 @@ CUDA_VISIBLE_DEVICES=0 python evaluate_tail.py --save_folder ./ckpts_atomic19/k2
     --dropout \
     --batch_size 512
 
-CUDA_VISIBLE_DEVICES=0 python evaluate_tail.py --save_folder ./ckpts_atomic19/k2/roberta_large \
+CUDA_VISIBLE_DEVICES=0 python evaluate_tail.py --save_folder ./ckpts_atomic/k2/roberta_large \
     --max_seq_length 32 \
     --temp 0.07 \
     --testfile ../preprocess/ATOMIC-Ind-test.txt \
